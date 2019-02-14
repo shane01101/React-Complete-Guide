0a6394329099788c58234853c069ecc1be7518d7
Shane Honanie <shaneh425@gmail.com>
2019-02-14 13:19:09 -0800

    Add Radium

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import Radium from "radium";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -49,7 +50,11 @@ class App extends Component {
 			font: "inherit",
 			border: "1px solid blue",
 			padding: "8px",
-			cursor: "pointer"
+			cursor: "pointer",
+			":hover": {
+				backgroundColor: "lightgreen",
+				color: "black"
+			}
 		};
 
 		let persons = null;
@@ -72,6 +77,10 @@ class App extends Component {
 			);
 
 			style.backgroundColor = "red";
+			style[":hover"] = {
+				backgroundColor: "lightred",
+				color: "black"
+			};
 		}
 
 		const classes = [];
@@ -97,4 +106,4 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default Radium(App);
