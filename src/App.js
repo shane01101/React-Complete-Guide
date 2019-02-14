d78a55e9c15236d9cfe5c1326d0b0ce57f4e0593
Shane Honanie <shaneh425@gmail.com>
2019-02-14 13:32:28 -0800

    Radium Media Queries

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import Radium from "radium";
+import Radium, { StyleRoot } from "radium";
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -78,7 +78,7 @@ class App extends Component {
 
 			style.backgroundColor = "red";
 			style[":hover"] = {
-				backgroundColor: "lightred",
+				backgroundColor: "salmon",
 				color: "black"
 			};
 		}
@@ -93,14 +93,16 @@ class App extends Component {
 		}
 
 		return (
-			<div className="App">
-				<h1>Hi, I'm a React App</h1>
-				<p className={classes.join(" ")}>This is really working!</p>
-				<button style={style} onClick={this.togglePersonsHandler}>
-					Toggle Persons
-				</button>
-				{persons}
-			</div>
+			<StyleRoot>
+				<div className="App">
+					<h1>Hi, I'm a React App</h1>
+					<p className={classes.join(" ")}>This is really working!</p>
+					<button style={style} onClick={this.togglePersonsHandler}>
+						Toggle Persons
+					</button>
+					{persons}
+				</div>
+			</StyleRoot>
 		);
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
 	}
