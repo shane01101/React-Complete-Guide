6d0323b94cd5b7597aadb5b2cc98da21cc6aeac5
Shane Honanie <shaneh425@gmail.com>
2019-02-13 16:10:35 -0800

    React Hooks Example

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const app = props => {
+	const [personsState, setPersonsState] = useState({
+		persons: [{ name: "Max", age: 28 }, { name: "Manu", age: 29 }, { name: "Steph", age: 26 }]
+	});
 
-export default App;
+	const [otherState] = useState("some other value");
+
+	console.log(personsState, otherState);
+
+	const switchNameHandler = () => {
+		// console.log("Was clicked");
+		setPersonsState({
+			persons: [{ name: "Maximilian", age: 28 }, { name: "Manu", age: 29 }, { name: "Steph", age: 27 }],
+			otherState: personsState.otherState
+		});
+	};
+
+	return (
+		<div className="App">
+			<h1>Hi, I'm a react app</h1>
+			<p>This is really working</p>
+			<button onClick={switchNameHandler}>Switch Name</button>
+			<Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+			<Person name={personsState.persons[1].name} age={personsState.persons[1].age}>
+				My Hobbies:Racing
+			</Person>
+			<Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
+		</div>
+	);
+};
+
+export default app;
