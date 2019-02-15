64c6d36c6f40dc06cc5297e4cacb6d1baad6751b
Shane Honanie <shaneh425@gmail.com>
2019-02-15 13:57:37 -0800

    Refactor

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,38 @@
+import React from "react";
+import classes from "./Cockpit.css";
+
+const Cockpit = props => {
+	const style = {
+		backgroundColor: "green",
+		color: "white",
+		font: "inherit",
+		border: "1px solid blue",
+		padding: "8px",
+		cursor: "pointer"
+	};
+	const classes = [];
+	let btnClass = "";
+	if (props.showPersons) {
+		btnClass = classes.Red;
+	}
+
+	if (props.persons.length <= 2) {
+		classes.push("red");
+	}
+
+	if (props.persons.length <= 1) {
+		classes.push("bold");
+	}
+
+	return (
+		<div className={classes.Cockpit}>
+			<h1>Hi, I'm a React App</h1>
+			<p className={classes.join(" ")}>This is really working!</p>
+			<button style={style} onClick={props.clicked}>
+				Toggle Persons
+			</button>
+		</div>
+	);
+};
+
+export default Cockpit;
